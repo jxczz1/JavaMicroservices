e950719ba56bb73154585207a4cf0e77ca7e0337
elpru <elprueba@pruebas.com>
2021-09-11 13:35:58 -0500

    add service eureka

---
+++ services-eureka/src/main/java/poli/serviceseureka/ServicesEurekaApplication.java
@@ -0,0 +1,13 @@
+package poli.serviceseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesEurekaApplication.class, args);
+    }
+
+}
