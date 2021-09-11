1defb96a0ca83e8475b77d40069cac3dded9dbb7
elpru <elprueba@pruebas.com>
2021-09-11 14:28:12 -0500

    add eurekaClient to microservices

--- services-users/src/main/java/co/com/poli/users/UsersApplication.java
+++ services-users/src/main/java/co/com/poli/users/UsersApplication.java
@@ -2,7 +2,9 @@ package co.com.poli.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UsersApplication {
 
